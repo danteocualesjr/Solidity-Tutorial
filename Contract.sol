14a29efc4b381c88243a4458d82814876e345561
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-23 14:41:14 +0800

    Added unsigned integers

--- Contract.sol
+++ Contract.sol
@@ -6,3 +6,10 @@ contract Contract {
     bool public a = true;
     bool public b = false;
 }
+
+// Unsigned integers
+contract Contract {
+    uint8 public a = 3;
+    uint16 public b = 256;
+    uint256 public sum = a + b;    
+}
