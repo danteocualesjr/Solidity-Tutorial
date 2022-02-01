e191244a15aec9ced0cd9418239cb55abf854dd0
Dante O. Cuales <danteocualesjr@gmail.com>
2022-02-01 10:39:13 +0800

    Is prime

--- Contract.sol
+++ Contract.sol
@@ -228,3 +228,18 @@ library Prime {
         return (x % y == 0);
     }    
 }
+
+// Is prime
+library Prime {
+    function dividesEvenly(uint x, uint y) public pure returns(bool) {
+        return (x % y == 0);
+    }
+    function isPrime(uint x) public pure returns(bool) {
+        for(uint i = 2; i <= x / 2; i++) {
+            if(dividesEvenly(x, i)) {
+                return false;
+            }
+        }
+        return true;
+    }    
+}
